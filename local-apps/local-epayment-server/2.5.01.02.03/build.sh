a339e44d2066c9e7b58a338c6ee4c1ee14d46611
worgflores <worgflores@gmail.com>
2021-01-26 15:49:47 +0800

    epayment-server: 2.5.01.02.03: updated

--- local-apps/local-epayment-server/2.5.01.02.03/build.sh
+++ local-apps/local-epayment-server/2.5.01.02.03/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[local-epayment-server] build..."
-docker rmi -f ramesesinc/local-epayment-server:2.5.01.02.02
-docker build -t ramesesinc/local-epayment-server:2.5.01.02.02 .
+docker rmi -f ramesesinc/local-epayment-server:2.5.01.02.03
+docker build -t ramesesinc/local-epayment-server:2.5.01.02.03 .
 echo ""
 echo "[local-epayment-server] finished."
