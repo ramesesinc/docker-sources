6b09e07272b544affbb797dedbcff7d105b82b23
elmonazareno <elmonazareno@gmail.com>
2021-04-26 11:37:16 +0800

    included assigned to me but not compeleted count in notifications

--- core/enterprise-common-server/255.05.001/modules/rameses-workflow-services.jar/sql/wf_taskcount.sql
+++ core/enterprise-common-server/255.05.001/modules/rameses-workflow-services.jar/sql/wf_taskcount.sql
@@ -7,3 +7,18 @@ WHERE bt.assignee_objid IS NULL
 AND sn.tracktime = 1
 AND sn.role IN ( ${roles} )
 GROUP BY bt.state
+
+UNION ALL
+
+SELECT 'mytask', COUNT(*) AS count 
+FROM ${processname}_task bt 
+INNER JOIN ${processname} pn ON pn.taskid = bt.taskid
+WHERE bt.assignee_objid = $P{userid}
+AND bt.state NOT IN ('start', 'end')
+
+[getMyTaskCount]
+SELECT COUNT(*) AS count 
+FROM ${processname}_task bt 
+INNER JOIN ${processname} pn ON pn.taskid = bt.taskid
+WHERE bt.assignee_objid = $P{userid}
+AND bt.state NOT IN ('start', 'end')
